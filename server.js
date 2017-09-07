a5823cfdf8215e9e81926662788205ceffca504a
gennop <31255043+gennop@users.noreply.github.com>
2017-09-08 01:34:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,21 +13,26 @@ app.get('/comunication.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'communication.html'));
 });
 
-app.get('/articleone.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+app.get('/articleone.htm', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.htm'));
 });
+
 app.get('/abhishek1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'abhishek1.html'));
 });
+
 app.get('/article-one',function(req,res){
     res.send('Hii...Yogendra Shukla...What are u doing bro.');
 });
+
 app.get('/article-two',function(req,res){
     res.send('Article two requested and will be served here');
 });
+
 app.get('/article-three',function(req,res){
     res.send('Article three requested and will be served here');
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
