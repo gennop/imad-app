4396bbdf7af3c04a97418464156f1b2e940e7221
gennop <31255043+gennop@users.noreply.github.com>
2017-09-10 01:04:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
+
+
+
+
 app.get('/index1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
 });
