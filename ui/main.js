0e71d38b7435bcf7ba7d70f60a1d143a8e62875f
gennop <31255043+gennop@users.noreply.github.com>
2017-09-09 13:03:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ console.log('Loaded!');
 //element=innerHTML('Hello Abhishek.Whats up.');
 //move the image
 var img=document.getElementById("madi");
-img.onclick=function(){
+img.onClick=function(){
     img.style.marginleft='100px';
 }
