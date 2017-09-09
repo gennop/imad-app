dba961c31ae8c72f1e3f07ddad1da44ceebe1920
gennop <31255043+gennop@users.noreply.github.com>
2017-09-10 02:54:10 +0530

    [imad-console] Updates ui/account.js

--- ui/account.js
+++ ui/account.js
@@ -1,1 +1,24 @@
+//counter code
+var button=document.getElementById('counter');
 
+button.onclick=function(){
+    
+    //create a request object 
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState === XMLHttpRequest.DONE ){
+            //take some action
+            if(request.status ===200){
+                var counter=request.responseText;
+                var span = socument.getElementBId('count');
+                span.innerHTML = counter.toString();
+            }
+        }      
+        //not done yet
+    };
+    //make the request
+    request.open('GET', 'http://srivastabhishek786.imad.hasura-app.io',true);
+    request.send(null);
+};
