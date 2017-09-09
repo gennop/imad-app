195d19889edfbec68f85032a9bd40fa9fbe32e91
gennop <31255043+gennop@users.noreply.github.com>
2017-09-09 12:58:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //var element = document.getElementByID('main-text');
 //element=innerHTML('Hello Abhishek.Whats up.');
 //move the image
-var img=document.getElementById('madi');
-function func(img){
-    img.style.height='1000px';
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginleft='100px'
 }
