9fa352ea45e2a33424c11aa37ffc61a34c133d80
gennop <31255043+gennop@users.noreply.github.com>
2017-09-17 21:24:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.get('/test-db', function(req,res){
        if(err){
            res.status(500).send(err.toString());
         }
-        else
-        res.send(JSON.toStringify(result)); 
+        else{
+        res.send(JSON.stringify(result)); 
        } 
     });
 });
