ccd08acd0f2697ba25651784c8a11790901ac273
gennop <31255043+gennop@users.noreply.github.com>
2017-09-17 23:38:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick= function(){
-    counter=counter+1;
-    var span =document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
-
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginleft= marginleft+10;
-    img.style.marginleft=marginleft + 'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-};
+console.log('Loaded!');
