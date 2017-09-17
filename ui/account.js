b16f6e4f6ee9b3be452ba6d7e81aa3dc7832f3ac
gennop <31255043+gennop@users.noreply.github.com>
2017-09-17 21:27:55 +0530

    Update account.js

--- ui/account.js
+++ ui/account.js
@@ -36,6 +36,6 @@ submit.onclick=function(){
     {
         list += '<li>' + names[i] + '</li>';
     }
-    var ul=document.getElementById('namelist');
+    var ul= document.getElementById('namelist');
     ul.innerHTML=list;
 }
