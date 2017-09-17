035216d5b4fc231e2d57397b18c21d9069d1dd8f
gennop <31255043+gennop@users.noreply.github.com>
2017-09-17 21:13:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool=require('pg'.pool);
+
+ var config = {
+     user: 'srivastabhishek786',
+     database: 'srivastabhishek786',
+     host: 'db.imad.hasura-app.io',
+     port:'5432',
+     password:process.env. DB_PASSWORD
+ };
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -9,15 +19,26 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config)
+
+app.get('/test-db', function(req,res){
+   //make a select request 
+   //return a response with the results
+    pool.query('SELECT * from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+        }
+        else
+        res.send(JSON.toStringify(result)); 
+       } 
+    });
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
    counter=counter+1;
    res.send(counter.toString());
 });
-
-
-
-
 
 app.get('/index1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
@@ -77,12 +98,6 @@ function createTemplate(data){
         ;
         return htmlTemplate;
 }
-
-    
-
-
-
-
 //my file
 
 app.get('/articleone', function (req, res) {
